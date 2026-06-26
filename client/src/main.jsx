b9cab376e0504f3303b7f6ef44b8bd11f29f9292
Guillermo Nunez <guillernc00@gmail.com>
2026-06-25 22:17:03 -0400

    feat: configure Stripe Elements provider
    closes #24

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 import App from './App.jsx';
 import './index.css';
+
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Elements stripe={stripePromise}>
+        <App />
+      </Elements>
     </BrowserRouter>
   </StrictMode>
 );
